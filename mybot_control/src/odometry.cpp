c8974efab5022c3a3e78608cce8de3a75eda1a2a
Andrea Antoniazzi <andreaenrico.amtoniazzi@gmail.com>
2018-07-25 11:29:16 +0200

    Improved scripts. TODO: enabling laserscan intensities which are actually set to 0

--- mybot_control/src/odometry.cpp
+++ mybot_control/src/odometry.cpp
@@ -2,6 +2,7 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include <gazebo_msgs/GetModelState.h>
+#include <tf/transform_datatypes.h>
 
 int main(int argc, char **argv)
 {
@@ -17,6 +18,8 @@ int main(int argc, char **argv)
   double y = 0.0;
   double th = 0.0;
 
+  double yaw, pitch, roll = 0.0;
+
   ros::Time current_time, last_time;
   current_time = ros::Time::now();
   last_time = ros::Time::now();
@@ -28,59 +31,67 @@ int main(int argc, char **argv)
   ros::Rate r(10);
   while (n1.ok())
   {
-    try{
-    ROS_INFO("ODOMETRY POSITIONS");
-    client.call(gms);
-    std::cout << "X: " << gms.response.pose.position.x << "\tY: " << gms.response.pose.position.y << "\tZ: " << gms.response.pose.position.z << "\n";
-    ros::spinOnce();
-    current_time = ros::Time::now();
+    try
+    {
+      ROS_INFO("ODOMETRY POSITIONS");
+      client.call(gms);
+      std::cout << "POSITIONS X: " << gms.response.pose.position.x << "\tY: " << gms.response.pose.position.y << "\tZ: " << gms.response.pose.position.z << "\n";
 
-    x = gms.response.pose.position.x;
-    y = gms.response.pose.position.y;
-    th = gms.response.pose.orientation.w;
+      ros::spinOnce();
+      current_time = ros::Time::now();
 
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+      tf::Quaternion q(gms.response.pose.orientation.x, gms.response.pose.orientation.y,
+                       gms.response.pose.orientation.z, gms.response.pose.orientation.w);
+      tf::Matrix3x3 m(q);
+      m.getRPY(roll, pitch, yaw);
+      
+      x = gms.response.pose.position.x;
+      y = gms.response.pose.position.y;
+      th = yaw;
 
-    //first, we'll publish the transform over tf
-    geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "chassis";
+      geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
-    odom_trans.transform.translation.x = x;
-    odom_trans.transform.translation.y = y;
-    odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom_quat;
+      //first, we'll publish the transform over tf
+      geometry_msgs::TransformStamped odom_trans;
+      odom_trans.header.stamp = current_time;
+      odom_trans.header.frame_id = "odom";
+      odom_trans.child_frame_id = "chassis";
 
-    //send the transform
-    odom_broadcaster.sendTransform(odom_trans);
+      odom_trans.transform.translation.x = x;
+      odom_trans.transform.translation.y = y;
+      odom_trans.transform.translation.z = 0.0;
+      odom_trans.transform.rotation = odom_quat;
 
-    //next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = "odom";
+      //send the transform
+      odom_broadcaster.sendTransform(odom_trans);
 
-    //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
+      //next, we'll publish the odometry message over ROS
+      nav_msgs::Odometry odom;
+      odom.header.stamp = current_time;
+      odom.header.frame_id = "odom";
 
-    //set the velocity
-    odom.child_frame_id = "chassis";
-    odom.twist.twist.linear.x = gms.response.twist.linear.x;
-    odom.twist.twist.linear.y = gms.response.twist.linear.y;
-    odom.twist.twist.angular.z = gms.response.twist.angular.z;
+      //set the position
+      odom.pose.pose.position.x = x;
+      odom.pose.pose.position.y = y;
+      odom.pose.pose.position.z = 0.0;
+      odom.pose.pose.orientation = odom_quat;
 
-    //publish the message
-    odom_pub.publish(odom);
+      //set the velocity
+      odom.child_frame_id = "chassis";
+      odom.twist.twist.linear.x = gms.response.twist.linear.x;
+      odom.twist.twist.linear.y = gms.response.twist.linear.y;
+      odom.twist.twist.angular.z = gms.response.twist.angular.z;
 
-    last_time = current_time;
-    r.sleep();
+      //publish the message
+      odom_pub.publish(odom);
+
+      last_time = current_time;
+      r.sleep();
     }
-    catch(const char* msg){
+    catch (const char *msg)
+    {
       std::cout << "ERROR OCCOURED DURING ODOMETRY CALCULATION! ERR: " << msg;
-      return;
+      return 0;
     }
   }
 }
--- mybot_control/src/teleop_mybot.cpp
+++ mybot_control/src/teleop_mybot.cpp
@@ -1,18 +1,27 @@
 #include "ros/ros.h"
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
 
 #define ANGULAR_RATIO 0.3
-#define VELOCITY_RATIO 4
+#define VELOCITY_RATIO 7
 
-int check1 = 0;
+int check1, check2 = 0;
 std_msgs::Float64 lin;
 std_msgs::Float64 ang;
 std_msgs::Float64 old_ang;
 std_msgs::Float64 old_lin;
+int linear_joy, angular_joy;
 
 void key_callback(const geometry_msgs::Twist::ConstPtr &value)
 {
+
+    if (!(abs(old_lin.data + value->linear.x) > 2))
+    {
+        lin.data = old_lin.data + value->linear.x;
+        old_lin.data = lin.data;
+    }
 
     lin.data = value->linear.x;
 
@@ -25,8 +34,22 @@ void key_callback(const geometry_msgs::Twist::ConstPtr &value)
     check1 = 1;
 }
 
+void JoyCallback(const sensor_msgs::Joy::ConstPtr &joy)
+{
+
+    geometry_msgs::Twist twist_bot;
+    twist_bot.angular.z = ANGULAR_RATIO * joy->axes[angular_joy];
+    twist_bot.linear.x = VELOCITY_RATIO * joy->axes[linear_joy];
+
+    lin.data = twist_bot.linear.x;
+    ang.data = twist_bot.angular.z;
+    check2 = 1;
+}
+
 int main(int argc, char **argv)
 {
+    angular_joy = 0;
+    linear_joy = 1;
     old_ang.data = 0.0;
     old_lin.data = 0.0;
 
@@ -37,7 +60,8 @@ int main(int argc, char **argv)
 
     ros::Publisher front = pub.advertise<std_msgs::Float64>("/mybot/front", 1000);
     ros::Publisher rear = pub.advertise<std_msgs::Float64>("/mybot/rear", 1000);
-    ros::Subscriber key = sub.subscribe("/turtle1/cmd_vel", 1000, key_callback);
+    ros::Subscriber joy = sub.subscribe<sensor_msgs::Joy>("joy", 10, JoyCallback);
+    ros::Subscriber key = sub.subscribe("turtle1/cmd_vel", 1000, key_callback);
 
     ros::Rate loop_rate(50);
     while (ros::ok())
@@ -55,9 +79,28 @@ int main(int argc, char **argv)
             catch (const char *msg)
             {
                 ROS_INFO("SOMETHING WENT WRONG!! are you sure that the controllers have strated correctly?");
+                std::cout << "ERR: " << msg;
             }
 
             check1 = 0;
+        }
+
+        if (check2 == 1)
+        {
+
+            try
+            {
+                std::cout << "VEL: " << lin.data << "\tANG: " << ang.data << "\n";
+                front.publish(ang);
+                rear.publish(lin);
+            }
+            catch (const char *msg)
+            {
+                ROS_INFO("SOMETHING WENT WRONG!! are you sure that the controllers have strated correctly?");
+                std::cout << "ERR: " << msg;
+            }
+
+            check2 = 0;
         }
 
         ros::spinOnce();
